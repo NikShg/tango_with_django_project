06869767d57a6d338139f969ec84c5ccdeedd8a1
NikShg <nikidde@outlook.com>
2019-02-07 01:06:28 +0000

    Update: include PageAdmin admin.py instead of separate

--- rango/admin.py
+++ rango/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from rango.models import Category, Page
 from polls.models import Choice,Question
 
-from rango.PageAdmin import PageAdmin
 
-
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    
 # register in admin panel
 admin.site.register(Category) 
 admin.site.register(Page, PageAdmin)
