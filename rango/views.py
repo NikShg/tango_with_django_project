2fbe33cf938e47c3740ed6fc05f78e34c499d574
NikShg <nikidde@outlook.com>
2019-02-15 02:04:54 +0000

    Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -12,14 +12,23 @@ from django.core.urlresolvers import reverse
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 #from unicodedata import category
 #def index(request): 
 #    return HttpResponse("Rango says hey there partner!<br/> <a href='/rango/about/'>About</a>.")
 
 def about (request):
     #return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>. ")
-    #context_dict ={'boldmessage': "This tutorial has been put together by 2207113"}
+    context_dict ={'boldmessage': "This tutorial has been put together by 2207113"}
     #return render(request, 'rango/about.html', context = context_dict)
+    if request.session.test_cookie_worked(): 
+       print("TEST COOKIE WORKED!") 
+       request.session.delete_test_cookie()
+       
+    request.session.set_test_cookie()
+    visitor_cookie_handler(request) 
+    context_dict['visits'] = request.session['visits']
+    
     print(request.method)  
     print(request.user) 
     return render(request, 'rango/about.html', {})
@@ -34,14 +43,17 @@ def index(request):
      #return render(request,'rango/index.html', context=context_dict)
      
      #get top 5 categories from database by likes, place in context_dict
-     
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5] 
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
     
-     #render the response
-    return render(request, 'rango/index.html', context_dict)
- 
+    visitor_cookie_handler(request) 
+    context_dict['visits'] = request.session['visits']
+    
+    response = render(request, 'rango/index.html', context=context_dict)
+   
+    return response
  # defin context dict, extract data from models, add data to dict
 def show_category(request, category_name_slug):
     #dict to pas to template
@@ -173,6 +185,28 @@ def restricted(request):
 def user_logout(request):
     logout(request)  
     return HttpResponseRedirect(reverse('index'))
+
+def get_server_side_cookie(request, cookie, default_val=None): 
+    val = request.session.get(cookie) 
+    if not val: 
+        val = default_val 
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request,'visits', '1'))
+    last_visit_cookie =get_server_side_cookie(request,'last_visit', str(datetime.now())) 
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], 
+                                        '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0: 
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
+ 
+     
+ 
          
   
                      
